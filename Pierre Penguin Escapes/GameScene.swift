9c6dfb81783d151ab3ad719c08adfeaefeb74f71
SaurabhSikka <drsaurabhsikka@gmail.com>
2015-12-09 11:59:12 +0530

    square sprites

--- Pierre Penguin Escapes/GameScene.swift
+++ Pierre Penguin Escapes/GameScene.swift
@@ -10,7 +10,29 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-       
+       // Create a sprite node or two
+        let mySprite = SKSpriteNode(color: UIColor.yellowColor(), size: CGSize(width: 50, height: 50))
+        let badSprite = SKSpriteNode(color: UIColor.whiteColor(), size: CGSize(width: 20, height: 20))
+        
+        // Give it a position relative to the parent node (the scene)
+        mySprite.position = CGPoint(x: 300, y: 300)
+        badSprite.position = CGPoint(x: 100, y: 100)
+        
+        // Add the sprite node to the parent node
+        self.addChild(mySprite)
+        self.addChild(badSprite)
+        
+        // Create some actions
+        let action0 = SKAction.moveTo(CGPoint(x: 200, y: 200), duration: 5)
+        let action1 = SKAction.scaleTo(4, duration: 4)
+        let action2 = SKAction.rotateByAngle(5, duration: 5)
+        let actionGroup = SKAction.group([action0, action2])
+        let actionSequence = SKAction.sequence([action1, action2])
+        
+        // Run the actions as singly, as a group, or in a sequence
+        badSprite.runAction(action0)
+        mySprite.runAction(actionSequence)
+        badSprite.runAction(actionGroup)
     }
     
 }
