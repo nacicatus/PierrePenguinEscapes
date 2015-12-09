593d10fda3bd4b15c2c796b1511a2f8cdbcdd2dd
SaurabhSikka <drsaurabhsikka@gmail.com>
2015-12-09 23:33:27 +0530

    camera zooming

--- Pierre Penguin Escapes/GameScene.swift
+++ Pierre Penguin Escapes/GameScene.swift
@@ -13,12 +13,15 @@ class GameScene: SKScene {
     // Create the world as a generice SKNode
     let world = SKNode()
     let ground = Ground()
-    
+    // store the center point of screen
+    var screenCenterY = CGFloat()
   
     // Create player
     let player = Player()
     
     override func didMoveToView(view: SKView) {
+        // Screen center
+        screenCenterY = self.size.height / 2
         
         // Blue sky
         self.backgroundColor = UIColor(red: 0.4, green: 0.6, blue: 0.94, alpha: 1)
@@ -51,11 +54,24 @@ class GameScene: SKScene {
     }
     
     override func didSimulatePhysics() {
+        var worldYPos: CGFloat = 0
         
-        let worldXPos = -(player.position.x * world.xScale - self.size.width / 2)
-        let worldYPos = -(player.position.y * world.yScale - self.size.height / 2)
+        // zoom the world as penguin flies higher
+        if player.position.y > screenCenterY {
+            let percentOfMaxHeight = (player.position.y - screenCenterY) / (player.maxHeight - screenCenterY)
+            let scaleSubtraction = (percentOfMaxHeight > 1 ? 1 : percentOfMaxHeight) * 0.6
+            let newScale = 1 - scaleSubtraction
+            
+            // The player is above half the screen size
+            // so adjust the world on the y-axis to follow:
+            world.yScale = newScale
+            world.xScale = newScale
+            
+            worldYPos = -(player.position.y * world.yScale - (self.size.height / 2))
+        }
+        let worldXPos = -(player.position.x * world.xScale - (self.size.width / 3))
         
-        // Center the bee in the world
+        // Move the world
         world.position = CGPoint(x: worldXPos, y: worldYPos)
         
     }
