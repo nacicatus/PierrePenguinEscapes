dc2648a6e98cf095a1297a537bdb9f710a7ae90b
SaurabhSikka <drsaurabhsikka@gmail.com>
2015-12-09 13:42:39 +0530

    bee flying back and forth!

--- Pierre Penguin Escapes/GameScene.swift
+++ Pierre Penguin Escapes/GameScene.swift
@@ -24,10 +24,26 @@ class GameScene: SKScene {
         // create an array from the frames in the atlas
         let beeFrames: [SKTexture] = [beeAtlas.textureNamed("bee.png"), beeAtlas.textureNamed("bee_fly.png")]
         //Create a new animation action
-        let flyAction = SKAction.animateWithTextures(beeFrames, timePerFrame: 0.05)
+        let flyAction = SKAction.animateWithTextures(beeFrames, timePerFrame: 0.14)
         let beeAction = SKAction.repeatActionForever(flyAction)
         
+        // Bee hover
         bee.runAction(beeAction)
+        
+        // move the bee left and right
+        let pathLeft = SKAction.moveByX(-200, y: -10, duration: 2)
+        let pathRight = SKAction.moveByX(200, y: 10, duration: 2)
+        
+        // flip the bee 
+        let flipTextureNegative = SKAction.scaleXTo(-1, duration: 0)
+        let flipTexturePositive = SKAction.scaleXTo(1, duration: 0)
+        
+        // bee flight sequence
+        let flightOfTheBee = SKAction.sequence([pathLeft, flipTextureNegative, pathRight, flipTexturePositive])
+        let neverEndingFlight = SKAction.repeatActionForever(flightOfTheBee)
+        
+        // tell the bee
+        bee.runAction(neverEndingFlight)
     }
     
 }
